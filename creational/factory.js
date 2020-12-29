acbca0cb8e295784648606fd32826493523b87cf
Shyrinha Vladyslav <shiringav@gmail.com>
2020-12-29 10:53:17 +0200

    Created directory

---
+++ creational/factory.js
@@ -0,0 +1,30 @@
+class Candy {
+
+    constructor(name, ccal, descriptions){
+        this.name = name;
+        this.ccal = ccal;
+        this.descriptions = descriptions;
+    }
+}
+
+class CandyFactory{
+    create(name) {
+        switch(name){
+            case 'caramel':
+                return new Candy('Caramel', 120, 'Candy with caramel');
+            case 'apple':
+                return new Candy('Apple', 92, 'Candy with apple');
+            case 'chocolate':
+                return new Candy('Dark Chocolate', 94, 'Candy with chocolate');
+            default:
+                console.log(`Type: ${name} not found`);
+                break;
+        }
+    }
+}
+
+const factory = new CandyFactory();
+
+console.log(factory.create('caramel'));
+console.log(factory.create('chocolate'));
+console.log(factory.create('apple'));
